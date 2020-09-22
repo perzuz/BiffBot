1f9189bc3bec8d49c67e2160ac4c8f1f41555508
perzuz <harrisscott@outlook.com>
2020-09-22 16:53:20 +0100

    Add: Sound playing functionality

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const Discord = require('discord.js');
 
 const client = new Discord.Client();
 
+var isReady = true;
+
 client.once('ready', () => {
     console.log('Hello Mcfly, Anybody Home!?');
 });
@@ -9,16 +11,23 @@ client.once('ready', () => {
 const prefix = '-'
 
 client.on('message', message => {
-    if(!message.content.startsWith(prefix) || message.author.bot) return;
-    
-    const args = message.content.slice(prefix.length).split(/ +/);
-    const command = args.shift().toLowerCase();
+    if(!message.content.startsWith(prefix) || message.author.bot || !isReady) return;
 
-    if(command === 'ping'){
-        message.channel.send("What are you looking at ButtHead!?");
-    }
+    isReady = false;
+
+    var voiceChannel = message.member.voice.channel;
+    voiceChannel.join().then(connection =>
+    {
+       const dispatcher = connection.play('resources/Butthead.mp3', { volume : 0.9});
+       
+       dispatcher.on("finish", end => {
+         voiceChannel.leave();
+         });
+
+     }).catch(err => console.log(err));
+     isReady = true;
+
 });
 
 // needs to be last line apparently
-client.login('NzU3NjQ0Njc3ODMzOTQ5MjQ0.X2jZlw.sfzLsbl6Wq3WLzi1ycxWqeDA884');
-
+client.login('NzU3NjQ0Njc3ODMzOTQ5MjQ0.X2jZlw.k7Eo2EDa2k4nQObjWP-XQ5AQO28');

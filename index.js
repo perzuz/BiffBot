26a4462bda6e6a8f3e831f937c96b8c2877aee48
Tony <t.gravano22@hotmail.co.uk>
2020-09-22 17:37:13 +0100

    Added reading from config. Attemtped to clean the code up

--- index.js
+++ index.js
@@ -1,33 +1,51 @@
-const Discord = require('discord.js');
+const Discord = require('discord.js')
 
-const client = new Discord.Client();
+const client = new Discord.Client()
 
-var isReady = true;
+var isReady = true
 
 client.once('ready', () => {
-    console.log('Hello Mcfly, Anybody Home!?');
-});
+	console.log('Hello Mcfly, Anybody Home!?')
+})
 
 const prefix = '-'
 
+// const dispatcher_on_start = () => {
+//     console.log('audio.mp3 is now playing!');
+// }
+
+// const dispatcher_on_finish = (voiceChannel) => {
+//     console.log('audio.mp3 has finished playing! Disconnecting from voice channel');
+//     voiceChannel.leave();
+// }
+
 client.on('message', message => {
-    if(!message.content.startsWith(prefix) || message.author.bot || !isReady) return;
+	if (!message.content.startsWith(prefix) || message.author.bot || !isReady)
+		return
 
-    isReady = false;
+	isReady = false
 
-    var voiceChannel = message.member.voice.channel;
-    voiceChannel.join().then(connection =>
-    {
-       const dispatcher = connection.play('resources/Butthead.mp3', { volume : 0.9});
-       
-       dispatcher.on("finish", end => {
-         voiceChannel.leave();
-         });
+	var voiceChannel = message.member.voice.channel
+	voiceChannel
+		.join()
+		.then(connection => {
+			const dispatcher = connection.play('resources/Butthead.mp3', {
+				volume: 0.9,
+			})
+			dispatcher.on('start', () => {
+				console.log('audio.mp3 is now playing!')
+			})
+			dispatcher.on('finish', () => {
+				voiceChannel.leave()
+			})
+			//dispatcher.on('start', dispatcher_on_start);
 
-     }).catch(err => console.log(err));
-     isReady = true;
-
-});
+			//dispatcher.on("finish", dispatcher_on_finish);
+		})
+		.catch(err => console.log(err))
+	isReady = true
+})
 
 // needs to be last line apparently
-client.login('NzU3NjQ0Njc3ODMzOTQ5MjQ0.X2jZlw.k7Eo2EDa2k4nQObjWP-XQ5AQO28');
+var config = require('./config.json')
+client.login(config.token)
